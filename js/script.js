1899b49a00709d4d0888dae46c82db8eacf13068
Denis Sidorenko <dongor7@gmail.com>
2018-11-06 17:55:51 +0300

    feat: add scroll event for line animation

--- js/script.js
+++ js/script.js
@@ -12,6 +12,17 @@ setTimeout(() => {
     document.querySelector('.infrastructure-svg').classList.add('draw');
 }, 2000);
 
+window.onscroll = function() {
+    if(window.pageYOffset + document.body.clientHeight > 800) {
+        console.log(window.pageYOffset + document.body.clientHeight);
+        document.querySelector('.solutions-svg').classList.add('draw');
+    }
+    if(window.pageYOffset + document.body.clientHeight > 1600) {
+        console.log(window.pageYOffset + document.body.clientHeight);
+        document.querySelector('.work-with-svg').classList.add('draw');
+    }
+};
+
 document.querySelector('.solutions-link a').addEventListener('click', function () {
     document.querySelector('.solutions-svg').classList.add('draw');
 });
