f460a0a099e0ed0b6275a80a8823f85d2ac4e53c
Denis Sidorenko <dongor7@gmail.com>
2018-11-08 17:00:18 +0300

    fix: script syntax

--- js/script.js
+++ js/script.js
@@ -9,32 +9,23 @@ for(let i = 0; i < links.length; i++){
     })
 }
 
-/*links.forEach(function(link) {
-    link.addEventListener('click', function () {
-        links.forEach(function(item) {
-            item.parentElement.classList.remove('active');
-        });
-        this.parentElement.classList.add('active');
-    })
-});*/
-
 setTimeout(function() {
-    document.querySelector('.infrastructure-svg').className += ' draw';
+    document.querySelector('.infrastructure-svg').className.baseVal += ' draw';
 }, 2000);
 
 window.onscroll = function() {
     if(window.pageYOffset + document.body.clientHeight > 800) {
-        document.querySelector('.solutions-svg').className += 'draw';
+        document.querySelector('.solutions-svg').className.baseVal += ' draw';
     }
     if(window.pageYOffset + document.body.clientHeight > 1600) {
-        document.querySelector('.work-with-svg').className += 'draw';
+        document.querySelector('.work-with-svg').className.baseVal += ' draw';
     }
 };
 
 document.querySelector('.solutions-link a').addEventListener('click', function () {
-    document.querySelector('.solutions-svg').className += 'draw';
+    document.querySelector('.solutions-svg').className.baseVal += ' draw';
 });
 
 document.querySelector('.work-with-link a').addEventListener('click', function () {
-    document.querySelector('.work-with-svg').className += 'draw';
+    document.querySelector('.work-with-svg').className.baseVal += ' draw';
 });
