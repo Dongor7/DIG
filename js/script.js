7be68ae0d2fc1335d35c4a0fef630c8efd6f7c60
Denis Sidorenko <dongor7@gmail.com>
2018-11-08 16:32:43 +0300

    fix: script ES version

--- js/script.js
+++ js/script.js
@@ -1,24 +1,22 @@
 let links = document.querySelectorAll('.sub-menu a');
-links.forEach(link => {
+links.forEach(function(link) {
     link.addEventListener('click', function () {
-        links.forEach(item => {
+        links.forEach(function(item) {
             item.parentElement.classList.remove('active');
         });
         this.parentElement.classList.add('active');
     })
 });
 
-setTimeout(() => {
+setTimeout(function() {
     document.querySelector('.infrastructure-svg').classList.add('draw');
 }, 2000);
 
 window.onscroll = function() {
     if(window.pageYOffset + document.body.clientHeight > 800) {
-        console.log(window.pageYOffset + document.body.clientHeight);
         document.querySelector('.solutions-svg').classList.add('draw');
     }
     if(window.pageYOffset + document.body.clientHeight > 1600) {
-        console.log(window.pageYOffset + document.body.clientHeight);
         document.querySelector('.work-with-svg').classList.add('draw');
     }
 };
