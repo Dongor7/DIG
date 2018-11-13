f151cad9331b1069b630edf871dd46bfa0d703eb
Denis Sidorenko <dongor7@gmail.com>
2018-11-13 13:38:33 +0300

    fix: main section float right

--- js/script.js
+++ js/script.js
@@ -9,29 +9,6 @@ for(let i = 0; i < links.length; i++){
     })
 }
 
-/*
-setTimeout(function() {
-    document.querySelector('.infrastructure-svg').className.baseVal += ' draw';
-}, 2000);
-
-window.onscroll = function() {
-    if(window.pageYOffset + document.body.clientHeight > 800) {
-        document.querySelector('.solutions-svg').className.baseVal += ' draw';
-    }
-    if(window.pageYOffset + document.body.clientHeight > 1600) {
-        document.querySelector('.work-with-svg').className.baseVal += ' draw';
-    }
-};
-
-document.querySelector('.solutions-link a').addEventListener('click', function () {
-    document.querySelector('.solutions-svg').className.baseVal += ' draw';
-});
-
-document.querySelector('.work-with-link a').addEventListener('click', function () {
-    document.querySelector('.work-with-svg').className.baseVal += ' draw';
-});
-*/
-
 var isSolutionDrowed = false;
 var isWorkWithDrowed = false;
 
